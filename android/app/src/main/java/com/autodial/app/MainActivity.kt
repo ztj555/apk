157ed96d8de9b4af3485de4876d2b775dc75e78d
ztj555 <ztj555@users.noreply.github.com>
2026-04-23 14:25:16 +0800

    fix: 添加缺失的PackageManager import

--- android/app/src/main/java/com/autodial/app/MainActivity.kt
+++ android/app/src/main/java/com/autodial/app/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
